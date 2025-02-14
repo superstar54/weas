17f995674e4f322db111f872c0dc290843745015
superstar54 <xingwang1991@gmail.com>
2025-02-14 05:53:31 +0100

    Add parser for xsf file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { AtomsViewer } from "./atoms/AtomsViewer.js";
 import { parseXYZ } from "./io/parserXYZ.js";
 import { parseCIF } from "./io/parserCif.js";
 import { parseCube } from "./io/parserCube.js";
+import { parseXSF } from "./io/parserXsf.js";
 import { elementAtomicNumbers } from "./atoms/atoms_data.js";
 
 // Export the modules to be publicly available
-export { WEAS, Specie, Atom, Atoms, AtomsViewer, parseXYZ, parseCIF, parseCube, elementAtomicNumbers };
+export { WEAS, Specie, Atom, Atoms, AtomsViewer, parseXYZ, parseCIF, parseCube, parseXSF, elementAtomicNumbers };
