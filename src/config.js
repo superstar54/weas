5379c367072b75456501bd23229c580db08c2a52
macaul_b <budmacaulay97@outlook.com>
2026-07-31 14:11:57 +0200

    config defaults to showing legend

--- src/config.js
+++ src/config.js
@@ -86,7 +86,7 @@ const defaultGuiConfig = {
     enabled: true, // Added this line to control timeline visibility
   },
   atomLegend: {
-    enabled: false,
+    enabled: true,
     position: "bottom-right", // Options: 'top-right', 'top-left', 'bottom-right', 'bottom-left'
   },
   meshLegend: {
